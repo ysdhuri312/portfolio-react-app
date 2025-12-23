2224f2913b38b060f20481e493b0f6ccc83f98fb
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-23 21:14:41 +0530

    Setup tailwind css and reset html

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @format */
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Opne Sans', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        text: {
+          primary: '#1A1A1A',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
