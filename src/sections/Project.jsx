3e05c8383812e4004c1c92ec182f797ab0322adc
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-26 18:54:29 +0530

    Update card height

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -10,7 +10,7 @@ const Project = () => {
     >
       {' '}
       <h3 className='text-4xl font-black text-center'>Featured My Works</h3>
-      <ul className='flex justify-center w-full gap-2 flex-wrap p-0'>
+      <ul className='flex justify-center w-full h-96 gap-2 flex-wrap p-0'>
         <li className='w-1/4 h-auto flex flex-col shadow-lg'>
           <img
             src={portfolio}
@@ -22,7 +22,7 @@ const Project = () => {
               My personal website - built with HTML, CSS, javascript and Deploy
               on GitHub Pages.
             </p>
-            <div className='mt-3'>
+            <div className='mt-4'>
               <a
                 href='https://ysdhuri312.github.io/dhuriyogesh.github.io/'
                 alt='live-link'
@@ -55,7 +55,7 @@ const Project = () => {
               build with Bootstrap, EJS, Express.js, MongoDB and deploy on
               Render.
             </p>
-            <div className='mt-3'>
+            <div className='mt-4'>
               <a
                 href='https://yelp-camp-i3wi.onrender.com/'
                 alt='live-link'
@@ -88,40 +88,7 @@ const Project = () => {
               build with Bootstrap, EJS, Express.js, MongoDB and deploy on
               Render.
             </p>
-            <div className='mt-3'>
-              <a
-                href='https://yelp-camp-i3wi.onrender.com/'
-                alt='live-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:opacity-90'
-                target='_blank'
-              >
-                Live
-              </a>
-              <a
-                href='https://github.com/ysdhuri312/yelp-camp.git'
-                alt='github-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:opacity-90'
-                target='_blank'
-              >
-                Github
-              </a>
-            </div>
-          </div>
-        </li>
-        <li className='w-1/4 h-auto flex flex-col shadow-lg'>
-          <img
-            src={yelpCamp}
-            className='w-full h-[50%] object-cover shadow-md'
-          />
-          <div className='m-2.5 h-auto flex flex-col justify-around'>
-            <h4 className='mt-2 font-bold text-md'>Yelp Camp</h4>
-            <p className='mt-2 text-md'>
-              A full-stack CRUD application to find nearest campground and if
-              you login then can add campground with images and description. It
-              build with Bootstrap, EJS, Express.js, MongoDB and deploy on
-              Render.
-            </p>
-            <div className='mt-3'>
+            <div className='mt-4'>
               <a
                 href='https://yelp-camp-i3wi.onrender.com/'
                 alt='live-link'
