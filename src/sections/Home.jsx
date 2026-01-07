5fba2d6ce5168bf5f4ef4b49706ca8e6ddcc60cd
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-07 15:36:28 +0530

    Complete mobile responsive home section

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -5,36 +5,45 @@ import resume from '../assets/pdf/Resume_Yogesh_Dhuri.pdf';
 
 const Home = () => {
   return (
-    <section id='home' className='container flex items-center py-2.5 mb-20'>
-      <div className='w-[60vw]'>
-        <h4 className='tracking-[7px] opacity-50 font-black mb-2.5'>
+    <section
+      id='home'
+      className='container flex flex-col sm:flex-row justify-between items-center py-2.5 mb-20'
+    >
+      <div className='items-center sm:w-[50vw] xs:my-9'>
+        <h4 className='xs:text-center sm:text-left tracking-[7px] opacity-50 font-black mb-2.5'>
           FULL STACK DEVELOPER
         </h4>
-        <h1 className='text-5xl mb-6 font-heading'>
+        <h1 className='xs:text-center sm:text-left text-4xl mb-3 font-heading lg:text-5xl lg:mb-6'>
           Hello, I&#39;m Yogesh Dhuri !
         </h1>
-        <p className='font-medium mb-9 pr-24 text-[17px]'>
+        <p className='xs:text-center sm:text-left font-medium mb-9 text-[17px] lg:pr-15'>
           I want transitioning into Junior Web Developer with hands-on
           experience in JavaScript, Node.js, and React. Seeking an entry-level
           software development role to apply my problem-solving skills and
           real-world business understanding in building scalable applications.
         </p>
-        <a
-          href='/#contact-me'
-          className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:bg-[#d94b2c]'
-        >
-          Notify Me
-        </a>
-        <a
-          href={resume}
-          className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:bg-[#d94b2c]'
-          target='_blank'
-        >
-          Resume
-        </a>
+        <div className='xs:text-center sm:text-left'>
+          <a
+            href='/#contact-me'
+            className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:bg-[#d94b2c]'
+          >
+            Notify Me
+          </a>
+          <a
+            href={resume}
+            className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:bg-[#d94b2c]'
+            target='_blank'
+          >
+            Resume
+          </a>
+        </div>
       </div>
-      <div className='w-[40vw]'>
-        <img src={home} className='m-9 h-90 w-96' alt='home-image' />
+      <div className='items-center sm:w-[50vw] xs:mt-9'>
+        <img
+          src={home}
+          className='my-9 ml-15 xs:size-65 sm:size-70 lg:size-90 xs:m-0'
+          alt='home-image'
+        />
       </div>
     </section>
   );
