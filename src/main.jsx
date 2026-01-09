486310dc8196b1f0914d7ccf41153935209433fc
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-09 17:11:09 +0530

    fix route casing for linux build

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
-import router from './routes/router.jsx';
+import router from './routes/router';
 import MetadataProvider from './utils/MetaDataProvider';
 import { Buffer } from 'buffer';
 globalThis.Buffer = Buffer;
