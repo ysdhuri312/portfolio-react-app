c6ec3950e4d6453fba1f075392e0ca7c84c0e02c
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-09 17:13:05 +0530

    Minor change

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
-import router from './routes/router';
 import MetadataProvider from './utils/MetaDataProvider';
 import { Buffer } from 'buffer';
 globalThis.Buffer = Buffer;
+import router from './routes/router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
